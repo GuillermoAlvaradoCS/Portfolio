51e3d814b960564cd46994692c1d23bb352abc6f
GuilleAlvarado <guillermo.alvarado@jala-foundation.org>
2024-04-26 22:18:40 -0400

    removed secret AGAIN

--- ChatGPT_clone/clone/src/openai.js
+++ ChatGPT_clone/clone/src/openai.js
@@ -1,15 +1,15 @@
-const { OpenAI } = require('openai')
-const openai = new OpenAI({apiKey: ""})
+// const { OpenAI } = require('openai')
+// const openai = new OpenAI({apiKey: ""})
 
-export async function sendMsgToOpenAI(message){
-    const res = await openai.completions.create({
-        model: 'text-davici-003',
-        prompt: message,
-        temperature: 0.7,
-        max_tokens: 256,
-        top_p: 1,
-        frequency_penalty: 0,
-        presense_penalty: 0
-    })
-    return res.data.choices[0].text
-}
+// export async function sendMsgToOpenAI(message){
+//     const res = await openai.completions.create({
+//         model: 'text-davici-003',
+//         prompt: message,
+//         temperature: 0.7,
+//         max_tokens: 256,
+//         top_p: 1,
+//         frequency_penalty: 0,
+//         presense_penalty: 0
+//     })
+//     return res.data.choices[0].text
+// }
