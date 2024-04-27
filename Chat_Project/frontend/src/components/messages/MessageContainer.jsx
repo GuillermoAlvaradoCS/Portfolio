4b47f98e3f876a0a0cf78f2bf5e9b072f4ff817a
GuilleAlvarado <guillermo.alvarado@jala-foundation.org>
2024-04-26 22:38:52 -0400

    profile pic container

--- Chat_Project/frontend/src/components/messages/MessageContainer.jsx
+++ Chat_Project/frontend/src/components/messages/MessageContainer.jsx
@@ -36,6 +36,9 @@ const NoChatSelected = () =>{
   return (
     <div className="flex items-center justify-center w-full h-full">
       <div className="px-4 text-center sm:text-lg md:text-xl text-gray-200 font-semibold flex flex-col items-center gap-2">
+        <div className="w-14 rounded-full">
+          <img src={authUser.profilePic} alt="user avatar" />
+        </div>
         <p>Welcome 👏 {authUser.fullname} ✨</p>
         <p>Select a chat to start messaging</p>
         <TiMessages className="text-3xl md:text-6xl text-center"/>
